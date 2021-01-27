ea4a1da984b9c6141d5f952a37ee26ece8b6ce0d
Thomas Gotwig <tgotwig@gmail.com>
2021-01-27 20:41:31 +0100

    ⬆️ Update Angular to version 11
    ng update @angular/core @angular/cli --force

--- src/app/play-dialog/play-dialog.component.spec.ts
+++ src/app/play-dialog/play-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 
 import { PlayDialogComponent } from './play-dialog.component'
 
@@ -6,7 +6,7 @@ describe('PlayDialogComponent', () => {
   let component: PlayDialogComponent
   let fixture: ComponentFixture<PlayDialogComponent>
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PlayDialogComponent],
     }).compileComponents()
--- src/app/rename-dialog/rename-dialog.component.spec.ts
+++ src/app/rename-dialog/rename-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 
 import { RenameDialogComponent } from './rename-dialog.component'
 
@@ -6,7 +6,7 @@ describe('RenameDeckDialogComponent', () => {
   let component: RenameDialogComponent
   let fixture: ComponentFixture<RenameDialogComponent>
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [RenameDialogComponent],
     }).compileComponents()
